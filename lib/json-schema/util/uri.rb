48f7a57cc40f5a246d586e1cdea0a4bdabf9a055
Peter Inglesby <peter.inglesby@gmail.com>
2015-03-04 00:36:22 +0000

    Cache Util::URI.normalized_uri

--- lib/json-schema/util/uri.rb
+++ lib/json-schema/util/uri.rb
@@ -2,6 +2,13 @@ module JSON
   module Util
     module URI
       def self.normalized_uri(uri)
+        @normalized_uris ||= Hash.new do |h, k|
+          h[k] = _normalized_uri(uri)
+        end
+        @normalized_uris[uri]
+      end
+
+      def self._normalized_uri(uri)
         uri = Addressable::URI.parse(uri) unless uri.is_a?(Addressable::URI)
         # Check for absolute path
         if uri.relative?
